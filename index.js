e1b948b522a30e3309933fc15693763bc23864f6
Charmaine Lee <me@charmaineklee.com>
2018-08-06 10:30:22 -0700

    Fix indent

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ app.listen(process.env.PORT || PORT, function() {
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.post('/', (req, res) => {
-var data = {form: {
-      token: process.env.SLACK_AUTH_TOKEN,
-      channel: "#general",
-      text: "Hi! :wave: \n I'm your new bot."
-    }};
-request.post('https://slack.com/api/chat.postMessage', data, function (error, response, body) {
-      // Sends welcome message
-      res.json();
-    });
+  var data = {form: {
+    token: process.env.SLACK_AUTH_TOKEN,
+    channel: "#general",
+    text: "Hi! :wave: \n I'm your new bot."
+  }};
+  request.post('https://slack.com/api/chat.postMessage', data, function (error, response, body) {
+    // Sends welcome message
+    res.json();
+  });
 });
